a8e87a09cf3a37e4a07117b7b49535388c3b1e52
MinexAutomation <minex.automation@gmail.com>
2019-10-17 16:25:32 -0700

    Added Bulgaria Rivet organization directory path provider implementation of the Alamania Rivet organization directory path provider interface.

---
+++ source/R5T.Alamania.Bulgaria/Code/Classes/BulgariaRivetOrganizationDirectoryPathProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using R5T.Bulgaria;
+using R5T.Costobocia;
+using R5T.Ostrogothia.Rivet;
+
+
+namespace R5T.Alamania.Bulgaria
+{
+    /// <summary>
+    /// Provides the Rivet organization directory path from the Dropbox directory path (provided by the codenamed Bulgaria project).
+    /// </summary>
+    public class BulgariaRivetOrganizationDirectoryPathProvider : IRivetOrganizationDirectoryPathProvider
+    {
+        public IDropboxDirectoryPathProvider DropboxDirectoryPathProvider { get; }
+        public IOrganizationStringlyTypedPathOperator OrganizationStringlyTypedPathOperator { get; }
+
+
+        public BulgariaRivetOrganizationDirectoryPathProvider(
+            IDropboxDirectoryPathProvider dropboxDirectoryPathProvider,
+            IOrganizationStringlyTypedPathOperator organizationStringlyTypedPathOperator)
+        {
+            this.DropboxDirectoryPathProvider = dropboxDirectoryPathProvider;
+            this.OrganizationStringlyTypedPathOperator = organizationStringlyTypedPathOperator;
+        }
+
+        public string GetRivetOrganizationDirectoryPath()
+        {
+            var dropboxDirectoryPath = this.DropboxDirectoryPathProvider.GetDropboxDirectoryPath();
+
+            var rivetOrganizationDropboxDirectoryPath = this.OrganizationStringlyTypedPathOperator.GetOrganizationDirectoryPath(dropboxDirectoryPath, RivetOrganization.Instance);
+            return rivetOrganizationDropboxDirectoryPath;
+        }
+    }
+}
