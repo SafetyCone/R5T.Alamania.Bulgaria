ac26a080863e215f0bcf3f329c0e91e8e9a4b3c2
MinexAutomation <minex.automation@gmail.com>
2020-02-18 22:00:42 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Alamania.Bulgaria/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bulgaria;
+using R5T.Costobocia;
+using R5T.Dacia;
+
+
+namespace R5T.Alamania.Bulgaria
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="BulgariaRivetOrganizationDirectoryPathProvider"/> implementation of <see cref="IRivetOrganizationDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddBulgariaRivetOrganizationDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<IDropboxDirectoryPathProvider> addDropboxDirectoryPathProvider,
+            ServiceAction<IOrganizationStringlyTypedPathOperator> addOrganizationStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<IRivetOrganizationDirectoryPathProvider, BulgariaRivetOrganizationDirectoryPathProvider>()
+                .RunServiceAction(addDropboxDirectoryPathProvider)
+                .RunServiceAction(addOrganizationStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="BulgariaRivetOrganizationDirectoryPathProvider"/> implementation of <see cref="IRivetOrganizationDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IRivetOrganizationDirectoryPathProvider> AddBulgariaRivetOrganizationDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<IDropboxDirectoryPathProvider> addDropboxDirectoryPathProvider,
+            ServiceAction<IOrganizationStringlyTypedPathOperator> addOrganizationStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<IRivetOrganizationDirectoryPathProvider>(() => services.AddBulgariaRivetOrganizationDirectoryPathProvider(
+                addDropboxDirectoryPathProvider,
+                addOrganizationStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
